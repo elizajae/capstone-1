43a99201e48ea6e400dbfc7017d831cfba9cc459
Eliza Christopher <elizac.business@gmail.com>
2023-12-27 14:37:18 -0500

    finished p1

--- app.py
+++ app.py
@@ -14,6 +14,47 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = 'bookify-secret'
 
+# XP dict that holds the XP values for page count milestones
+
+XP = {
+    0: 0,
+    50: 10,
+    100: 20,
+    150: 30,
+    200: 40,
+    250: 50,
+    300: 60,
+    350: 70,
+    400: 80,
+    450: 90,
+    500: 100,
+    550: 110,
+    600: 120,
+    650: 130,
+    700: 140,
+    750: 150,
+    800: 160,
+    850: 170,
+    900: 180,
+    950: 190,
+    1000: 200,
+}
+
+# Dict that holds level thresholds
+
+
+def get_level_threshold(level):
+    return 100 * level + 100
+
+
+def determine_level(xp):
+    level = 0
+    while xp >= get_level_threshold(level):
+        xp -= get_level_threshold(level)
+        level += 1
+    return level
+
+
 connect_db(app)
 
 with app.app_context():
@@ -23,9 +64,50 @@ with app.app_context():
 @app.route("/")
 def root():
 
-    lists = UserList.get_user_lists(session["user_id"])
+    if "user_id" in session:
 
-    return render_template("home.html", explore=False, lists=lists)
+        # get most common subject if they have any books in progress
+        progress = ProgressEntry.get_progress_entries(session["user_id"])
+
+        lists = UserList.get_user_lists(session["user_id"])
+
+        if progress:
+            # Get Book Meta
+            books = []
+
+            for entry in progress:
+                BASE_URL = f"https://www.googleapis.com/books/v1/volumes/{entry.book_id}"
+
+                res = requests.get(BASE_URL)
+
+                if res.status_code == 200:
+                    data = res.json()
+                    books.append(data)
+
+            if books:
+                # Get Subject
+                subjects = []
+
+                for book in books:
+                    subjects.extend(book["volumeInfo"].get("categories", []))
+
+                if subjects:
+                    subject = max(set(subjects), key=subjects.count)
+                    print(subject)
+                    BASE_URL = "https://www.googleapis.com/books/v1/volumes"
+
+                    params = {"q": f"subject:{subject}", "maxResults": 8}
+
+                    res = requests.get(BASE_URL, params=params)
+
+                    if res.status_code == 200:
+                        data = res.json()
+                        books = data.get("items", [])
+                        return render_template("home.html", explore=False, books=books, lists=lists)
+
+        return render_template("home.html", explore=False, lists=lists)
+
+    return render_template("home.html", explore=False, lists=[])
 
 
 @app.route("/explore")
@@ -173,6 +255,20 @@ def track_progress_for_book(book_id):
                 session["user_id"], book_id, current_page, total_pages)
             db.session.commit()
 
+        # Check if user has reached a milestone
+        current_xp = User.query.get(session["user_id"]).experience
+
+        new_xp_value = 0
+
+        for page_number in sorted(XP.keys()):
+            if current_page >= page_number:
+                new_xp_value = XP[page_number]
+
+        # Update the user's experience
+        User.query.get(session["user_id"]
+                       ).experience = current_xp + new_xp_value
+        db.session.commit()
+
         return redirect(url_for("book", book_id=book_id))
 
     return redirect(url_for("track_progress", book_id=book_id))
@@ -180,7 +276,6 @@ def track_progress_for_book(book_id):
 
 @app.route("/track/<book_id>")
 def track_progress(book_id):
-    form = TrackProgressForm()
 
     progress = ProgressEntry.get_progress_entries(session["user_id"])
 
@@ -189,6 +284,8 @@ def track_progress(book_id):
     for entry in progress:
         if entry.book_id == book_id:
             progress_for_current_book = entry
+
+    form = TrackProgressForm(progress=progress_for_current_book)
 
     return render_template("track_progress.html", form=form, progress=progress_for_current_book, book_id=book_id)
 
@@ -299,3 +396,49 @@ def create_list():
         return redirect(url_for("root"))
 
     return render_template("new_list.html", form=form)
+
+
+@app.route("/list/view/<list_id>")
+def list(list_id):
+
+    list = UserList.query.get_or_404(list_id)
+
+    books_raw = UserListBook.get_user_list_books(list_id)
+
+    books_meta = []
+
+    for book in books_raw:
+        BASE_URL = f"https://www.googleapis.com/books/v1/volumes/{book.book_id}"
+
+        res = requests.get(BASE_URL)
+
+        if res.status_code == 200:
+            data = res.json()
+            books_meta.append(data)
+
+    return render_template("list.html", list=list, books=books_meta)
+
+
+@app.route("/profile")
+def profile():
+
+    tracked_books = ProgressEntry.get_progress_entries(session["user_id"])
+
+    books_meta = []
+
+    for entry in tracked_books:
+        BASE_URL = f"https://www.googleapis.com/books/v1/volumes/{entry.book_id}"
+
+        res = requests.get(BASE_URL)
+
+        if res.status_code == 200:
+            data = res.json()
+            books_meta.append(data)
+
+    xp = User.query.get(session["user_id"]).experience
+
+    level = determine_level(xp)
+
+    xp_to_next_level = get_level_threshold(level) - xp
+
+    return render_template("profile.html", experience=xp, level=level, xp_to_next_level=xp_to_next_level, books=books_meta)
